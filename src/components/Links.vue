c8f0b90c87766a2a6df7be295d343ad26194551c
r33ya <62694790+r33ya@users.noreply.github.com>
2025-07-29 23:13:19 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,9 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Book } from "@vicons/fa"; // 注意使用正确的类别
+import { AddLinkRound, LinkOffRound } from "@vicon/material";
+import { Activity } from "@vicon/tabler"
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -64,12 +66,10 @@ const siteLinksList = computed(() => {
 // 网站链接图标
 const siteIcon = {
   Blog,
-  Cloud,
-  CompactDisc,
-  Compass,
   Book,
-  Fire,
-  LaptopCode,
+  AddLinkRound,
+  LinkOffRound,
+  Activity
 };
 
 // 链接跳转
